41f7898de50822f513109933fac26569957d4df4
ahmed saminaka <ahmedsaminaka@gmail.com>
2023-03-08 15:28:53 -0800

    files 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,28 +1,21 @@
 #include "main.h"
 
 /**
- * wildcmp - compares two strings and returns 1 if the strings
- *           can be considered identical, otherwise return 0.
- * @s1: string to compare to
- * @s2: string with wild character
+ * _strlen_recursion - Returns the length of a string.
+ * @s: string to measured.
  *
- * Return: On success 1, else 0
+ * Return: The length of the string.
  */
-int wildcmp(char *s1, char *s2)
+
+int _strlen_recursion(char *s)
 {
-	if (*s1 == '\0' && *s2 == '\0')
-		return (1);
+	int len = 0;
 
-	if (*s1 == *s2)
-		return (wildcmp(s1 + 1, s2 + 1));
-
-	if (*s2 == '*')
+	if (*s)
 	{
-		if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
-			return (0);
-		if (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2))
-			return (1);
+		len++;
+		len = len +  _strlen_recursion(s + 1);
 	}
 
-	return (0);
+	return (len);
 }
